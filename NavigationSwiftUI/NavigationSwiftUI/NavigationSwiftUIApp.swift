095362920a009e9ef1a11613d51aedf825153ccc
anchao-lu <2252571428@qq.com>
2021-04-12 16:20:00 +0800

    NavigationSwiftUI initial

---
+++ NavigationSwiftUI/NavigationSwiftUI/NavigationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSwiftUIApp.swift
+//  NavigationSwiftUI
+//
+//  Created by monkey on 2021/4/12.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
